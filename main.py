ef871feda294cf0fa8d67afad173726e41268875
Donald Lee <51865552+DonaldKLee@users.noreply.github.com>
2021-08-15 06:46:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Project Name: Leadeo
+By: Donald Lee
+Start date: Aug 14th 2021
+"""
+
+from flask import Flask, render_template
+import random
+
+app = Flask(  # Create a flask app
+	__name__,
+	template_folder='templates',  # Name of html file folder
+	static_folder='static'  # Name of directory for static files
+)
+
+@app.route('/')  # '/' for the default page
+def home():
+	return render_template("index.html")
+
+
+
+
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
